2564813888a2a36f2d7e56e5b57619b27b977251
Philippe Auriou <p.auriou@live.fr>
2023-06-27 22:03:20 +0200

    save

---
+++ src/Class1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaOrganize.Net
+{
+    internal class WorkerHostedService : IHostedService
+    {
+        public WorkerHostedService(
+            IServiceProvider serviceProvider,
+            IHostApplicationLifetime appLifetime,
+            IConfiguration configuration)
+        {
+            
+
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
